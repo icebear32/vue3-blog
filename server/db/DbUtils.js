4ca89530e555ef212fb61e7c7207ab254a909a2d
icebear32 <1727693137@qq.com>
2023-12-02 14:29:17 +0800

    服务端-Promise封装部分方法

--- server/db/DbUtils.js
+++ server/db/DbUtils.js
@@ -1,8 +1,27 @@
 const sqlite3 = require("sqlite3").verbose()
 const path = require("path")
 const GenId = require("../utils/SnowFlake")
+const { rejects } = require("assert")
 
 var db = new sqlite3.Database(path.join(__dirname, "blog.sqlite3"))
 const genid = new GenId({ WorkerId: 1 })
+
+db.async = {}
+
+db.async.all = (sql, params) => {
+    return new Promise((resolve, reject) => {
+        db.all(sql, params, (err, rows) => {
+            resolve({ err, rows })
+        })
+    })
+}
+
+db.async.run = (sql, params) => {
+    return new Promise((resolve, reject) => {
+        db.run(sql, params, (err, rows) => {
+            resolve({ err, rows })
+        })
+    })
+}
 
 module.exports = { db, genid }
--- server/routers/TestRouter.js
+++ server/routers/TestRouter.js
@@ -3,12 +3,20 @@ const express = require("express")
 const router = express.Router()
 const { db, genid } = require("../db/DbUtils")
 
-router.get("/test", (req, res) => {
-    db.all("select * from `admin`",[],(err,rows)=>{
+router.get("/test", async (req, res) => {
+    db.all("select * from `admin`", [], (err, rows) => {
         console.log(rows)
     })
+
+    db.async.all("select * from `admin`", []).then((res) => {
+        console.log(res)
+    })
+
+    let out = await AbortController.async.all("select * from `admin`", [])
+
     res.send({
         id: genid.NextId(),
+        out
     })
 })
 
