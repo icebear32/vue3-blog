e26d11cb9db34941b9b2b0650fe86beaadd4018d
icebear32 <1727693137@qq.com>
2023-12-05 16:34:39 +0800

    前端-项目创建和模块安装

---
+++ client/src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+/**
+ * axios：npm install axios
+ * pinia：npm install pinia
+ * sass：npm install sass
+ * vue-router：npm install vue-router@4
+ * naive-ui：npm i -D naive-ui
+ * naive-ui 字体：npm i -D vfonts
+ * wangeditor：npm install @wangeditor/editor-for-vue@next --save
+ */
+
+
+createApp(App).mount('#app')
