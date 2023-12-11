17ff707720ebbd287c6fab3ae6a12395dad61f18
icebear32 <1727693137@qq.com>
2023-12-11 17:09:11 +0800

    前端-首页页面制作

--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { ref, inject, onMounted, computed } from 'vue'
+import { ref, reactive, inject, onMounted, computed } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
-import Dashboard from './dashboard/Dashboard.vue';
 
 const router = useRouter()
 const route = useRoute()
@@ -13,9 +12,40 @@ const axios = inject("axios")
 const selectedCategory = ref(0)
 const categoryOptions = ref([])
 
+const blogListInfo = ref([]) // 文章列表
+// 分页 列表个数
+const pageInfo = reactive({
+    page: 1,
+    pageSize: 3,
+    pageCount: 0,
+    count: 0,
+    keyword: ""
+})
+
 onMounted(() => {
     loadCategorys()
+    loadBlogs()
 })
+
+// 显示文章列表
+const loadBlogs = async (page = 0) => {
+    if (page != 0) {
+        pageInfo.page = page
+    }
+    let res = await axios.get(`/blog/search?keyword=${pageInfo.keyword}&page=${pageInfo.page}&pageSize=${pageInfo.pageSize}`)
+    let temp_rows = res.data.data.rows
+    for (let row of temp_rows) {
+        row.content += "..."
+        let d = new Date(row.create_time)
+        row.create_time = `${d.getFullYear()}年${d.getMonth() + 1}月${d.getDate()}日`
+    }
+    blogListInfo.value = temp_rows
+
+    pageInfo.count = res.data.data.count // 列表总条数
+    pageInfo.pageCount = parseInt(pageInfo.count / pageInfo.pageSize) + (pageInfo.count % pageInfo.pageSize > 0 ? 1 : 0) // 总页数
+
+    console.log(res)
+}
 
 const categoryName = computed(() => {
     let selectOption = categoryOptions.value.find((options) => { return options.value == selectedCategory.value })
@@ -45,7 +75,7 @@ const dashboard = () => {
 <template>
     <div class="container">
         <div class="nav">
-            <div @click="HomePage">首页</div>
+            <div @click="homePage">首页</div>
             <div>
                 <n-popselect v-model:value="selectedCategory" :options="categoryOptions" trigger="click">
                     <div>分类<span>{{ " " + categoryName }}</span></div>
@@ -53,7 +83,25 @@ const dashboard = () => {
             </div>
             <div @click="dashboard">后台</div>
         </div>
-        <n-divider></n-divider>
+        <n-divider class="n-divider" />
+        <div class="list">
+            <n-space class="search">
+                <n-input v-model:value="pageInfo.keyword" :style="{ width: '500px' }" placeholder="请输入关键字" />
+                <n-button type="primary" ghost @click="loadBlogs(0)"> 搜索</n-button>
+            </n-space>
+            <div v-for="(blog, index) in blogListInfo" style="margin-bottom: 15px;">
+                <n-card :title="blog.title">
+                    {{ blog.content }}
+                    <template #footer>
+                        <n-space align="center">
+                            <div>发布时间：{{ blog.create_time }}</div>
+                        </n-space>
+                    </template>
+                </n-card>
+            </div>
+            <n-pagination @update:page="loadBlogs" v-model:page="pageInfo.page" :page-count="pageInfo.pageCount" />
+        </div>
+        <n-divider class="n-divider" />
         <div class="footer">
             <div>xx</div>
             <div>xxxx</div>
@@ -62,7 +110,7 @@ const dashboard = () => {
 </template>
 
 <style lang="scss" scoped>
-.catainer {
+.container {
     width: 1200px;
     margin: 0 auto;
 }
@@ -71,8 +119,6 @@ const dashboard = () => {
     display: flex;
     font-size: 20px;
     padding-top: 20px;
-    margin-left: 100px;
-    margin-right: 100px;
     color: #64676a;
 
     div {
@@ -89,9 +135,22 @@ const dashboard = () => {
     }
 }
 
+.list {
+    height: 580px;
+
+    .search {
+        padding-bottom: 10px;
+    }
+}
+
 .footer {
     text-align: center;
     line-height: 25px;
     color: #64676a;
+}
+
+.n-divider {
+    margin-top: 10px;
+    margin-bottom: 10px;
 }
 </style>
